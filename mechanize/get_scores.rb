455b938e5269e306277af95ce0625e1d87dd4a63
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-04-29 20:24:07 -0700

    Add script to scrape the Euler site and get people's scores

---
+++ mechanize/get_scores.rb
@@ -0,0 +1,31 @@
+require 'mechanize'
+require 'yaml'
+
+config = YAML.load_file('secrets.yml')
+
+agent = Mechanize.new
+
+login_pg = agent.get("https://projecteuler.net/login")
+
+login_pg.form_with(:name => "login_form") do |f|
+  f.username = config['username']
+  f.password = config['password']
+  f['login'] = "Login"            #STUPID STUPID STUPID
+end.submit
+
+friends_pg = agent.get("https://projecteuler.net/friends")
+
+usernames = friends_pg.links_with(:href => /progress=/).map { |l| l.text }
+
+progress = {}
+
+usernames.each do |user|
+  agent.get("https://projecteuler.net/progress=#{user}")
+  status = agent.page.search('#progress_bar_section h3').text
+  status =~ /Solved (\d*)/
+  progress[user] = $1
+end
+
+progress.each do |user, score|
+  puts "#{user} has solved #{score}"
+end
