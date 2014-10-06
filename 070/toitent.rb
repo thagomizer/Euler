17437797702a8e62e76d2b43e8cf052ef337e9d8
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-10-05 17:55:11 -0700

    Totient

--- 070/toitent.rb
+++ 070/toitent.rb
@@ -13,4 +13,69 @@
 # Find the value of n, 1 < n < 107, for which phi(n) is a permutation of
 # n and the ratio n/phi(n) produces a minimum
 
+require 'pp'
+require 'prime'
 
+class Fixnum
+  $totients = {2 => 1.to_f}
+  $factorizations = {1 => [1], 2 => [2]}
+
+  def totient
+    return 1 if self == 1
+
+    t = 0
+    case
+    when Prime.prime?(self)
+      t = self -1
+    when self.even?
+      m = self / 2
+      if m.even?
+        t = m.totient  * 2
+      else
+        t = m.totient
+      end
+    else
+      factors = self.prime_factorization.uniq
+      t = factors.inject(self) { |p, n| p *= (1 - (1.0/n)) }.to_i
+    end
+
+    $totients[self] = t.to_f
+  end
+
+  def prime_factorization
+    return [self] if Prime.prime?(self)
+    return $factorizations[self] if $factorizations[self]
+
+    root = Math.sqrt(self).floor
+    root.downto(1) do |x|
+      if self % x == 0 then
+        f = x.prime_factorization + (self / x).prime_factorization
+        $factorizations[self] = f
+        return f
+      end
+    end
+  end
+
+  def permutation?(n)
+    self.to_s.split('').sort == n.to_s.split('').sort
+  end
+end
+
+min = {:phi_n => 1000, :n => 0}
+
+2.upto(107) do |n|
+  phi = n.totient
+  next unless phi.to_i.permutation?(n)
+  if min[:phi_n] > n/phi
+    min[:phi_n] = n
+    min[:n] = n
+  end
+end
+
+pp min
+
+# {:phi_n=>63, :n=>63}
+
+# real	0m0.124s
+# user	0m0.072s
+# sys	0m0.050s
