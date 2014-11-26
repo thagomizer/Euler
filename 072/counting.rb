2e5bf04e3855f6b0bfbde7639db7886975a9a92a
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-11-25 20:37:43 -0800

    NOT FAST ENOUGH....GRRR

--- 072/counting.rb
+++ 072/counting.rb
@@ -18,60 +18,34 @@ require 'pp'
 require 'prime'
 
 class Fixnum
-  $totients = {1 => 1, 2 => 1}
-  $factors = {1 => [1, 1], 2 => [2, 1]}
-  $real_factors = { 1 => [], 2 => [[2, 1]]}
-
-  def totient
-    return $totients[self] if $totients[self]
-
-    $totients[self] = case
-                      when Prime.prime?(self)
-                        self - 1
-                      when self.even?
-                        m = self / 2
-                        if m.even?
-                          m.totient * 2
-                        else
-                          m.totient
-                        end
-                      else
-                        m, n = simple_factors
-                        d = m.gcd(n)
-                        (m.totient * n.totient * d/d.totient)
-                      end
-  end
-
-  def simple_factors
-    $factors[self] ||= begin
-                         root = Math.sqrt(self).floor
-                         root.downto(1) do |x|
-                           next unless self % x == 0
-                           break [x, self / x]
-                         end
-                       end
-  end
+  $factors = {}
 
   def µ
-    $real_factors[self] ||= Prime.prime_division(self)
     if self.square_free?
-      return 1 if $real_factors[self].length.even?
-      return -1
+      self.factors.length.even? ? 1 : -1
     else
-      return 0
+      0
     end
   end
 
   def square_free?
-    $real_factors[self] ||= Prime.prime_division(self)
-    $real_factors[self].none? { |_, pow| pow > 1 }
+    self.factors.none? { |_, pow| pow > 1 }
+  end
+
+  def factors
+    $factors[self] ||= Prime.prime_division(self)
   end
 end
 
-(1..20).each { |n| puts n.µ }
+def count_fractions n
+  (1..n).inject(0) { |sum, k| sum + (k.µ * (n/k)**2) } / 2
+end
 
-# def count_fractions (denom)
-#   (1..denom).map { |n| n.totient }.inject(:+) - 1
-# end
+puts count_fractions(1_000_000)
 
-# puts count_fractions(1_000_000)
+# NOT FAST ENOUGH...GRRR
+# 303963552391
+
+# real	1m24.769s
+# user	1m24.196s
+# sys	0m0.496s
