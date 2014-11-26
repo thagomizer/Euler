d8ee31ea48492700ba58484f397309bdc9a835d9
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-11-25 19:58:56 -0800

    WIP

--- 072/counting.rb
+++ 072/counting.rb
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+require 'pp'
+
 # Consider the fraction, n/d, where n and d are positive integers. If
 # n < d and HCF(n,d)=1, it is called a reduced proper fraction.
 
@@ -12,20 +15,63 @@
 # How many elements would be contained in the set of reduced proper
 # fractions for d <= 1,000,000?
 
-def count_fractions (denom)
-  count = 0
+require 'prime'
 
-  (2..denom).each do |d|
-    (1...d).each do |n|
-      if n.gcd(d) == 1
-        count += 1
-      end
+class Fixnum
+  $totients = {1 => 1, 2 => 1}
+  $factors = {1 => [1, 1], 2 => [2, 1]}
+  $real_factors = { 1 => [], 2 => [[2, 1]]}
+
+  def totient
+    return $totients[self] if $totients[self]
+
+    $totients[self] = case
+                      when Prime.prime?(self)
+                        self - 1
+                      when self.even?
+                        m = self / 2
+                        if m.even?
+                          m.totient * 2
+                        else
+                          m.totient
+                        end
+                      else
+                        m, n = simple_factors
+                        d = m.gcd(n)
+                        (m.totient * n.totient * d/d.totient)
+                      end
+  end
+
+  def simple_factors
+    $factors[self] ||= begin
+                         root = Math.sqrt(self).floor
+                         root.downto(1) do |x|
+                           next unless self % x == 0
+                           break [x, self / x]
+                         end
+                       end
+  end
+
+  def µ
+    $real_factors[self] ||= Prime.prime_division(self)
+    if self.square_free?
+      return 1 if $real_factors[self].length.even?
+      return -1
+    else
+      return 0
     end
   end
 
-  count
+  def square_free?
+    $real_factors[self] ||= Prime.prime_division(self)
+    $real_factors[self].none? { |_, pow| pow > 1 }
+  end
 end
 
-(1..100).each do |x|
-  puts "#{x}: #{count_fractions(x)}"
-end
+(1..20).each { |n| puts n.µ }
+
+# def count_fractions (denom)
+#   (1..denom).map { |n| n.totient }.inject(:+) - 1
+# end
+
+# puts count_fractions(1_000_000)
